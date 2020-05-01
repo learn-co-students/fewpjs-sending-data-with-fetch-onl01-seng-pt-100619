0dbebfca8f933f04d16f9043697e01973266538e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-01 17:39:15 +0200

    add headers required by servers so the requests are not rejected

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Add your code here
 // add configuration object values to make send data through a json file
-
+// add headers required by servers so the requests are not rejected
 configurationObject = {
     method: "POST",
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
       }
   };
 

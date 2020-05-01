51992750950718156980fd6d56ea011ee287c392
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-01 17:35:33 +0200

    add configuration object values to make send data through a json file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your code here
+// add configuration object values to make send data through a json file
+
+configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+      }
+  };
+
+fetch(destinationURL, configurationObject);

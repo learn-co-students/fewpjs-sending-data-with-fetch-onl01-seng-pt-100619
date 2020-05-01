00ecbc81d70e5b64d5ffeebc1840e767bc3c6d65
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-01 18:29:20 +0200

    create a function with a fetch request inside and the values of the data to send as arguments of the function and set them to the body by setting the body inside the scope of the submitData function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Add your code here
 // add configuration object values to make send data through a json file
 // add headers required by servers so the requests are not rejected
+function submitData(name, email){
 
 let formData = {
-    dogName: "Byron",
-    dogBreed: "Poodle"
+    name: name,
+    email: email
   };
    
   let configObj = {
@@ -16,16 +17,46 @@ let formData = {
     // add content-type and accept headers set to json and make the json a string to make the request
     body: JSON.stringify(formData)
   };
+
+    fetch("http://localhost:3000/users", configObj)
+    //     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+}
+
+
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
    
-  fetch("http://localhost:3000/dogs", configObj)  // add fetch set to post to send the data to our server of choosing and it behaves like a form posting to the database
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(object) {
-        console.log(object);
-    })
-    .catch(function(error) {
-        alert("Bad things! Ragnarők!");
-        console.log(error.message);
-    });
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     // add content-type and accept headers set to json and make the json a string to make the request
+//     body: JSON.stringify(formData)
+//   };
+   
+//   fetch("http://localhost:3000/dogs", configObj)  // add fetch set to post to send the data to our server of choosing and it behaves like a form posting to the database
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
 // add .catch to return errors. for example if the post method is not included in the configuration object then it cant have a body or data to send, only to recieve

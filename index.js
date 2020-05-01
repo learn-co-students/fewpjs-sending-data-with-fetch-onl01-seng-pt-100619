ea9cda444c6cef43f4722947064fee59c514ac3b
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-01 19:02:21 +0200

    append text node from object to the body element and also append error to the body element from catch as its callback function

--- index.js
+++ index.js
@@ -3,33 +3,36 @@
 // add headers required by servers so the requests are not rejected
 function submitData(name, email){
 
-let formData = {
-    name: name,
-    email: email
-  };
-   
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    // add content-type and accept headers set to json and make the json a string to make the request
-    body: JSON.stringify(formData)
-  };
+    let formData = {
+        name: name,
+        email: email
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        // add content-type and accept headers set to json and make the json a string to make the request
+        body: JSON.stringify(formData)
+    };
+    
+        return fetch("http://localhost:3000/users", configObj)
+            .then(response => response.json())
+            .then(function(object) {
+                let objectId = document.createTextNode(`${object.id}`)
+                let body = document.querySelector("body");
+                body.appendChild(objectId) // append text node from object to the body element 
+            })
+            .catch( function(error) {
+                let errorMessage = document.createTextNode(`${error.message}`)
+                let body = document.querySelector("body"); 
+                body.appendChild(errorMessage); // append error to the body element from catch as its callback function 
+            });
+};
+       
 
-    fetch("http://localhost:3000/users", configObj)
-    //     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(object) {
-//         console.log(object);
-//     })
-//     .catch(function(error) {
-//         alert("Bad things! Ragnarők!");
-//         console.log(error.message);
-//     });
-}
 
 
 
